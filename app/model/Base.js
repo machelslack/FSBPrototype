d4e07e3aa8dbdb5cb426edf1b1d1e6ab8ff06f00
FSB Technology <FSB@Machels-MacBook-Pro.local>
2018-03-31 18:45:43 +0100

    first commit sencha app

---
+++ app/model/Base.js
@@ -0,0 +1,7 @@
+Ext.define('FSBPrototype.model.Base', {
+    extend: 'Ext.data.Model',
+
+    schema: {
+        namespace: 'FSBPrototype.model'
+    }
+});
---
+++ app/model/Personnel.js
@@ -0,0 +1,7 @@
+Ext.define('FSBPrototype.model.Personnel', {
+    extend: 'FSBPrototype.model.Base',
+
+    fields: [
+        'name', 'email', 'phone'
+    ]
+});
